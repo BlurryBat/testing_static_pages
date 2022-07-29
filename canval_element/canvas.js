f2706c0611fcf449ca6ba1dace891e529e1d0de5
BlurryBat <53352897+BlurryBat@users.noreply.github.com>
2022-07-29 12:19:17 +0530

    Update canvas.js

--- canval_element/canvas.js
+++ canval_element/canvas.js
@@ -15,22 +15,28 @@
       var context = canvas.getContext('2d');
       img.onload = function() {
         shrink_factor = 1
-        img_new = document.createElement("img");
-        img_new.setAttribute("id", "new_img")
-
+        quality=1
+        //img_new.setAttribute("id", "new_img")
         do {
+          img_new = document.createElement("img");
           canvas.width = img.width / shrink_factor;
           canvas.height = img.height / shrink_factor;
           context.drawImage(img, 0, 0, canvas.width, canvas.height);
           //context.drawImage(img,0,0,img.width,img.height,0,0,100,100);
           //var pixelBuf = context.getImageData(0,0,10,100);
-          img_new.src = canvas.toDataURL("image/jpeg", .1);
+          img_new.src = canvas.toDataURL("image/jpeg", quality);
           document.getElementById('body').appendChild(img_new);
-          console.log('size : ' + img_new.src.length / 1024 + 'KB');
+          console.log('size : ' + img_new.src.length / 1024 + 'KB ');
+          console.log( img_new.src);
           //document.getElementById('body').appendChild(canvas);
-          shrink_factor=shrink_factor*2;
+          shrink_factor=shrink_factor*1.2;
+          if(quality > 0){
+            quality-=.1;
+          }
         }
-        while(img_new.src.length>200*1024);
+        while(img_new.src.length>100*1024);
+        //console.log(img_new.src);
+
       }
 
     });
