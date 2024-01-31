7f072178fa003b34e945c521bb24c7f939a54898
BlurryBat <53352897+BlurryBat@users.noreply.github.com>
2024-02-01 00:37:30 +0530

    Update canvas.js

--- canval_element/canvas.js
+++ canval_element/canvas.js
@@ -5,7 +5,7 @@
       const reader = new window.FileReader()
       size = input.files[0].size;
       console.log(input.files[0].name); 
-      document.getElementById('file_name').innerHTML += input.files[0].name;
+      document.getElementById('file-name').innerHTML += input.files[0].name;
       let img = document.createElement('img')
       img.setAttribute("id","theimage")
       reader.readAsDataURL(input.files[0])
